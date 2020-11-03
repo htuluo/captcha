f5b1084c8722f1c12de10b0895e48bc7e85b0348
luoleiming <luoleiming@dangdang.com>
2020-11-03 17:43:39 +0800

    输出图片流

--- service/springboot/src/main/java/com/anji/captcha/demo/controller/ReadImageController.java
+++ service/springboot/src/main/java/com/anji/captcha/demo/controller/ReadImageController.java
@@ -2,9 +2,12 @@ package com.anji.captcha.demo.controller;
 
 import com.sun.deploy.net.HttpResponse;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -17,10 +20,11 @@ import java.io.*;
  * @date ：Created in 2020/10/30
  * @description： //TODO
  */
-@Controller
+@RestController
 public class ReadImageController {
 
-    @RequestMapping("get")
+    @RequestMapping(value = "get",produces = {MediaType.IMAGE_PNG_VALUE,MediaType.IMAGE_JPEG_VALUE})
+    @ResponseBody()
     public void readImage(HttpServletRequest request, HttpServletResponse response) throws IOException {
         File file = new File("D:/srcImg.png");
         byte[] bytes = FileCopyUtils.copyToByteArray(file);
@@ -28,7 +32,7 @@ public class ReadImageController {
 //        response=((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
         response.setContentType("image/png");
         OutputStream stream=response.getOutputStream();
-        stream.write(bytes);
+        stream.write(null);
         stream.flush();
         stream.close();
 
